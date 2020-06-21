d3f69c602e0858bd0696f9f6e5240b74cfc91965
knsinta <kholisohns16@gmail.com>
2020-06-21 22:38:02 +0700

    Movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D rb;
+    public Animator anim;
 
-    // Update is called once per frame
-    void Update()
+    private void Update() 
     {
-        
+        if(Input.GetKey(KeyCode.A)) 
+        {
+            rb.velocity = new Vector2(-3, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+            anim.SetBool("berlari", true);
+        }  
+
+        else if(Input.GetKey(KeyCode.D)) 
+        {
+            rb.velocity = new Vector2(3, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+            anim.SetBool("berlari", true);
+        }
+
+        else
+        {
+            anim.SetBool("berlari", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+        }   
     }
 }
