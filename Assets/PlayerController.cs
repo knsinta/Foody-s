d76216f6ede17fc0b429949b48f16699ca102813
knsinta <kholisohns16@gmail.com>
2020-06-22 07:05:05 +0700

    Animation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,33 +4,81 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public Animator anim;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Collider2D coll;
+    
+    private enum Status {diam, berlari, melompat, jatuh}
+    private Status status = Status.diam;
+    
+    [SerializeField]private LayerMask ground;
+    [SerializeField]private float speed = 3f;
+    [SerializeField]private float jumpforce = 5f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
 
     private void Update() 
     {
-        if(Input.GetKey(KeyCode.A)) 
+        Movement(); 
+        KecepatanStatus(); 
+        anim.SetInteger("status", (int)status); 
+    }
+    
+    private void Movement()
+    {
+    float hDirection = Input.GetAxis("Horizontal");
+
+        if(hDirection < 0) 
         {
-            rb.velocity = new Vector2(-3, rb.velocity.y);
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
-            anim.SetBool("berlari", true);
+            // anim.SetBool("berlari", true);
         }  
 
-        else if(Input.GetKey(KeyCode.D)) 
+        else if(hDirection > 0) 
         {
-            rb.velocity = new Vector2(3, rb.velocity.y);
+            rb.velocity = new Vector2(speed, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
-            anim.SetBool("berlari", true);
+            // anim.SetBool("berlari", true);
+        }
+
+        if(Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+            status = Status.melompat;
+        }
+    }
+    private void KecepatanStatus()
+    {
+        if(status == Status.melompat)
+        {
+            if(rb.velocity.y < .1f)
+            {
+                status = Status.jatuh;
+            }
+        }
+
+        else if(status == Status.jatuh)
+        {
+            if(coll.IsTouchingLayers(ground))
+            {
+                status = Status.diam;
+            }
+        }
+
+        else if(Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            status = Status.berlari;
         }
 
         else
         {
-            anim.SetBool("berlari", false);
+            status = Status.diam;
         }
-
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 10f);
-        }   
     }
 }
